6d39bbebeb141771427c29d88bce5331dc61c0e9
Nadia Bajwa <nadiabajwa@Nadias-MacBook-Pro.local>
2015-12-02 14:32:45 -0600

    changing sketch tags to contact, adding jquery to the navbar and centering the heading of the last section

--- index.js
+++ index.js
@@ -10,4 +10,25 @@ $(document).ready(function() {
   
 });
 
+    $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash,
+    $target = $(target);
+
+    // Scroll to inline links
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+      window.location.hash = target;
+    });
+  });
+
+
+
+
+
+
 });
+
+
