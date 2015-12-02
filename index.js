c0dcc9c92837dbeabb94835ab6f76f848d2f9715
Nadia Bajwa <nadiabajwa@Nadias-MacBook-Pro.local>
2015-12-01 22:34:52 -0600

    require jquery library, down arrow working with html tags

---
+++ index.js
@@ -0,0 +1,28 @@
+// $(document).ready(function() {
+//     console.log("I reached here!");
+
+//     $('.svg').animate({
+//     scrollTop: ($('#about-me').offset().top) 
+//     },500);
+// });
+
+
+
+
+
+    // var target = $( $(this) );
+
+   //  if( target.length ) {
+   //      event.preventDefault();
+   //      $('about-me').stop().animate({
+   //          scrollTop: target.offset().top
+   //      }, 1000);
+   //  }
+   // //  console.log("it reached here!");
+   // $("about-me").animate({
+   //      scrollTop: $( $(this).attr('href')).offset().top
+   //  }, 500);
+
+// });
+
+// });
