7787ad093dd475e3206321b7c931f533d15101f9
Nadia Bajwa <nadiabajwa@Nadias-MacBook-Pro.local>
2015-12-01 23:37:06 -0600

    Javascript for the arrow is working

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-// $(document).ready(function() {
-//     console.log("I reached here!");
+$(document).ready(function() {
+    
+   $("#arrow").on('click', function(event) {
+      console.log("I reached here!");
 
-//     $('.svg').animate({
-//     scrollTop: ($('#about-me').offset().top) 
-//     },500);
-// });
+   $('html, body').animate({
+    scrollTop: $("#about-me").offset().top
+}, 1000);
 
+  
+});
 
-
-
-
-    // var target = $( $(this) );
-
-   //  if( target.length ) {
-   //      event.preventDefault();
-   //      $('about-me').stop().animate({
-   //          scrollTop: target.offset().top
-   //      }, 1000);
-   //  }
-   // //  console.log("it reached here!");
-   // $("about-me").animate({
-   //      scrollTop: $( $(this).attr('href')).offset().top
-   //  }, 500);
-
-// });
-
-// });
+});
