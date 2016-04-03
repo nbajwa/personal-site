eca2e47bbc93af9a095b83723f9b299065ab7e7b
Nadia Bajwa <nadia.bajwa88@gmail.com>
2016-04-03 18:22:40 -0500

    fixing more grammer issure

--- index.js
+++ index.js
@@ -16,8 +16,27 @@ $(document).ready(function() {
 
     $('.color-word').css('color', random_color);
 
-
-
+        //settings
+        var fadeSpeed = 200, fadeTo = 0.5, topDistance = 30;
+        var topbarME = function() { $('.block-menu').fadeTo(fadeSpeed,1); }, topbarML = function() { $('.block-menu').fadeTo(fadeSpeed,fadeTo); };
+        var inside = false;
+        //do
+        $(window).scroll(function() {
+            position = $(window).scrollTop();
+            if(position > topDistance && !inside) {
+                //add events
+                topbarML();
+                $('.block-menu').bind('mouseenter',topbarME);
+                $('.block-menu').bind('mouseleave',topbarML);
+                inside = true;
+            }
+            else if (position < topDistance){
+                topbarME();
+                $('.block-menu').unbind('mouseenter',topbarME);
+                $('.block-menu').unbind('mouseleave',topbarML);
+                inside = false;
+            }
+        });
 
 
 
